cf9156ecaf817599c60c823b3d01125c13716e70
Luong Bui <manh.luong@gmail.com>
2018-05-19 11:43:50 +0100

    Added another rest call.

--- test/rest_test.dart
+++ test/rest_test.dart
@@ -1,15 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-void loadData(String url) async {
-  print('GET from: ' + url);
+void loadData(String url) async { 
   final response = await http.get(url);
+  print('\nGET from: ' + url); 
   var jsonResponse = JSON.decode(response.body);
   JsonEncoder encoder = new JsonEncoder.withIndent('  ');
-  print(encoder.convert(jsonResponse));
+  print(encoder.convert(jsonResponse) + '\n');
 }
 
 void main() {
   loadData('https://stardewvalleywiki.com/mediawiki/api.php?action=query&format=json&list=allpages');
+  loadData('https://stardewvalleywiki.com/mediawiki/api.php?action=opensearch&search=cow&limit=10&format=json');
 }
 
